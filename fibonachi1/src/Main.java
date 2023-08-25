08ffbde2cdaa5b036556b2bb9976384feb13854d
Andrey3681 <117780962+Andrey3681@users.noreply.github.com>
2023-08-24 21:18:03 -0500

    agrege los modulos para los ejercicios en clase para q

--- fibonachi1/src/Main.java
+++ fibonachi1/src/Main.java
@@ -3,10 +3,11 @@ public class Main {
     public static int fib(int n){
         if (n <= 1) return n;
         else
-            System.out.println(n);
+            System.out.println(n); System.out.println("izquierda"+ (n-1));
+        System.out.println("derecha"+(n-2));
             return fib(n-1)+fib(n-2);    //condición recursiva
     }
     public static void main(String[] args) {
-        System.out.println(fib(21));
+        System.out.println(fib(5));
     }
 }
